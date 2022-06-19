a062873457a80cfecb64c8fb4b5e7666e5f4609d
Lucid <67615874+Lucid-017@users.noreply.github.com>
2022-06-19 13:59:09 +0100

    fixed footer links

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -4,12 +4,12 @@ const Footer = () => {
   return (
     <div className='w-2/4 mx-auto text-center my-8'>
       <p ><span className='text-ash-800'>Built by  : </span>
-         <a className='animate' href="https://twitter.com/UchennaOnwuliri">
+         <a className='animate' target='_blank' rel='noreferrer' href="https://twitter.com/UchennaOnwuliri">
           Onwuliri Uchenna Austin
         </a>
       </p>
       <p className='text-sm mt-2'>Credits:
-        <a className='animate' href="https://www.football-data.org/">
+        <a className='animate' target='_blank' rel='noreferrer' href="https://www.football-data.org/">
           Football data API
         </a>
       </p>
